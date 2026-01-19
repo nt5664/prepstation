420937329019b97b3f8543f4aac4046e958c09d4
Norbert <79476468+nt5664@users.noreply.github.com>
2026-01-19 23:38:23 +0100

    Update table-schema.ts

--- app/_utils/form-schemas/table-schema.ts
+++ app/_utils/form-schemas/table-schema.ts
@@ -1,6 +1,12 @@
 import z from "zod";
 import { FormFieldConfig } from "@/app/_utils/form-schemas/form-fields-base";
 
+const extraColumnSchema = z.object({
+  id: z.string(),
+  name: z.string().min(1),
+  key: z.string(),
+});
+
 export const tableFormSchema = z.object({
   tableStub: z
     .string()
@@ -14,14 +20,23 @@ export const tableFormSchema = z.object({
     .string()
     .min(1, "The title is required.")
     .max(64, "The max length is 64 characters"),
-  startDate: z
-    .date("Invalid date. A valid start date is required.")
-    .nonoptional("Start date is required."),
+  startDate: z.iso.datetime().nonoptional("Start date is required"),
   transitionTime: z
     .number("Invalid value, a positive number is required.")
-    .nonnegative("The length cannot be negative")
-    .nonoptional("Transition time is required."),
-  extraColumns: z.string().optional(),
+    .nonnegative("The length cannot be negative"),
+  channel: z
+    .string()
+    .max(25, "The max length is 25 characters")
+    .regex(
+      /^[a-zA-Z0-9][a-zA-Z0-9_]+$/,
+      "The channel name can only contain alphanumeric characters and underscores.",
+    )
+    .optional(),
+  website: z
+    .url("The website must be a URL")
+    .max(64, "The max length is 64 characters")
+    .optional(),
+  extraColumns: z.array(extraColumnSchema),
 });
 
 export type TableFormSchema = z.infer<typeof tableFormSchema>;
@@ -29,6 +44,8 @@ export type TableFormSchema = z.infer<typeof tableFormSchema>;
 export const tableFormFields: FormFieldConfig<TableFormSchema> = {
   tableStub: { id: "tableStub", maxLength: 32 },
   tableTitle: { id: "tableTitle", maxLength: 64 },
+  channel: { id: "channel", maxLength: 25 },
+  website: { id: "website", maxLength: 64 },
   startDate: { id: "startDate" },
   transitionTime: { id: "transitionTime" },
   extraColumns: { id: "extraColumns" },
