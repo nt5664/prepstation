4288e47f0e003ee9b9ce0c560ab48b10fadfe6a2
Norbert <79476468+nt5664@users.noreply.github.com>
2026-02-01 21:18:52 +0100

    Update route.ts

--- app/api/auth/[...better-auth]/route.ts
+++ app/api/auth/[...better-auth]/route.ts
@@ -1,5 +1,9 @@
 import { auth } from "@/app/_lib/auth";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
 export async function GET(req: Request) {
   return await auth.handler(req);
 }
