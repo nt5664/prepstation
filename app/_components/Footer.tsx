d36f5729a9b0c1ffa16c2cfa31f37cbd00e07ec7
Norbert <79476468+nt5664@users.noreply.github.com>
2026-02-16 15:12:00 +0100

    Footer has the mandatory cookie notification

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,9 +3,16 @@ import PreferenceEditor from "@/app/_components/PreferenceEditor";
 export default function Footer() {
   return (
     <footer className="mt-auto h-12 px-2 py-1.5 border-t-3 border-teal-800 bg-gray-800">
-      <div className="flex gap-2">
-        <p className="text-sm">&copy; 2026 PrepStation.</p>
-        <PreferenceEditor />
+      <div className="flex flex-col">
+        <div className="flex gap-2">
+          <p className="text-sm">&copy; 2026 PrepStation.</p>
+          <PreferenceEditor />
+        </div>
+
+        <div className="text-center text-xs font-semibold">
+          This app only uses essential cookies required for logins and saving
+          user preferences.
+        </div>
       </div>
     </footer>
   );
